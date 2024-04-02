d22463c9032847dae6d62dd364380e86fb5850c8
Arpan Mahanty <arpanmahanty01@gmail.com>
2024-04-03 04:29:03 +0530

    electron init

---
+++ client/main.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'FileWave',
+    width: 1400,
+    height: 800,
+    resizable: false,
+    icon: './renderer/public/icon.png',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  const startUrl = url.format({
+    pathname: path.join(__dirname, './renderer/build/index.html'),
+    protocol: 'file',
+  });
+
+  // mainWindow.loadURL(startUrl);
+  mainWindow.loadURL(`http://localhost:${process.env.FRONTEND_PORT}`);
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
