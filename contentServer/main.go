32ecac207a8a7ffad4a36adf52557945ae04e4ea
Arpan Mahanty <arpanmahanty01@gmail.com>
2024-04-04 18:18:51 +0530

    fixed CORS

--- contentServer/main.go
+++ contentServer/main.go
@@ -8,53 +8,63 @@ import (
 	"sync"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type connTracker struct {
-    net.Listener
-    conns int
-    mu    sync.Mutex
+	net.Listener
+	conns int
+	mu    sync.Mutex
 }
+
 //TODO check if this counts the mnumber of users connected
 func (t *connTracker) Accept() (net.Conn, error) {
-    conn, err := t.Listener.Accept()
-    if err != nil {
-        return nil, err
-    }
-    t.mu.Lock()
-    t.conns++
-    t.mu.Unlock()
-    return conn, nil
+	conn, err := t.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+	t.mu.Lock()
+	t.conns++
+	t.mu.Unlock()
+	return conn, nil
 }
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    configs.ConnectDB()
+	// Enable CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders: "*",
+	}))
 
-    routes.ContentRouter(app)
+	configs.ConnectDB()
 
-    ln, _ := net.Listen("tcp", ":4000")
-    tln := &connTracker{Listener: ln}
+	routes.ContentRouter(app)
 
-    go func() {
-        for {
-            conn, err := tln.Accept()
-            if err != nil {
-                // handle error
-            }
-            go func(c net.Conn) {
-                fmt.Println(tln.conns)
-                // handle connection
-                tln.mu.Lock()
-                if tln.conns > 20 {
-                    // connections more than 20
-                }
-                tln.mu.Unlock()
-                c.Close()
-            }(conn)
-        }
-    }()
+	ln, _ := net.Listen("tcp", ":4000")
+	tln := &connTracker{Listener: ln}
 
-    app.Listener(tln)
+	go func() {
+		for {
+			conn, err := tln.Accept()
+			if err != nil {
+                fmt.Println("Error accepting connection:", err)
+                continue
+			}
+			go func(c net.Conn) {
+				fmt.Println(tln.conns)
+				// handle connection
+				tln.mu.Lock()
+				if tln.conns > 20 {
+					// connections more than 20
+				}
+				tln.mu.Unlock()
+				c.Close()
+			}(conn)
+		}
+	}()
+
+	app.Listener(tln)
 }
